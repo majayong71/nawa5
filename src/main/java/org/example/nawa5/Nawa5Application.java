d599a8a8efda4ad2ca95cfa39c1e32a2522eb864
majayong71 <com4032647@gmail.com>
2025-05-06 16:35:59 +0900

    프로젝트 빌드 확인

---
+++ src/main/java/org/example/nawa5/Nawa5Application.java
@@ -0,0 +1,13 @@
+package org.example.nawa5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nawa5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nawa5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nawa5/Nawa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nawa5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nawa5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
