af502ca1f5e505f319ed5e171eea8c7216ef9515
majayong71 <wotkd2647@naver.com>
2025-06-28 23:56:05 +0900

    Refactor code to follow code conventions

--- src/main/java/org/example/nawa5/service/UserService.java
+++ src/main/java/org/example/nawa5/service/UserService.java
@@ -4,21 +4,33 @@ import org.example.nawa5.domain.User;
 
 public interface UserService {
 
-    /** 회원 가입 **/
-    void register (String email, String password, String nickname);
+    /**
+     * 회원 가입
+     **/
+    void register(String email, String password, String nickname);
 
-    /** 로그인 **/
-    String login (String email, String password);
+    /**
+     * 로그인
+     **/
+    String login(String email, String password);
 
-    /** 회원 정보 조회 **/
+    /**
+     * 회원 정보 조회
+     **/
     User get(Long id);
 
-    /** 회원 정보 수정  **/
+    /**
+     * 회원 정보 수정
+     **/
     void update(Long id, String nickname);
-    
-    /** 회원 패스워드 수정 **/
-    void updatePassword (Long id, String password);
 
-    /** 회원 탈퇴 **/
+    /**
+     * 회원 패스워드 수정
+     **/
+    void updatePassword(Long id, String password);
+
+    /**
+     * 회원 탈퇴
+     **/
     void delete(Long id);
 }
